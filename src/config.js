ef08fa36e49c1ec84f9ab31050522bd776cf4462
Jah'swill Emmanuel <jahswill4jahs@gmail.com>
2025-03-14 20:17:59 +0100

    Add console log for API_KEY in config.js to verify correct logging

--- src/config.js
+++ src/config.js
@@ -1,4 +1,5 @@
 export const API_KEY = import.meta.env.VITE_API_KEY;
+console.log("API Key:", API_KEY); // Check if it logs correctly
 export const ACCESS_TOKEN = import.meta.env.VITE_ACCESS_TOKEN;
 export const BASE_URL = import.meta.env.VITE_BASE_URL;
 export const IMAGE_BASE_URL = import.meta.env.VITE_IMAGE_BASE_URL;
