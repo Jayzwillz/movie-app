2694225af1eb1f501024e60fedb6fa4b1f162bef
Jah'swill Emmanuel <jahswill4jahs@gmail.com>
2025-03-14 20:25:50 +0100

    Update API_KEY in config.js to a hardcoded value for testing purposes

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
-export const API_KEY = import.meta.env.VITE_API_KEY;
+export const API_KEY = "82f0eb51b8d6e6bb582d64ba891533b4";
 console.log("API Key:", API_KEY); // Check if it logs correctly
 export const ACCESS_TOKEN = import.meta.env.VITE_ACCESS_TOKEN;
 export const BASE_URL = import.meta.env.VITE_BASE_URL;
